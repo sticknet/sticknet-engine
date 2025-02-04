a8eb08091f3fc77d9f19e9681abea85dda1e99c9
Omar Basem <omarbasem.0101@gmail.com>
2025-02-04 17:09:06 +0400

    lint

--- .platform/hooks/prebuild/00_install_dependencies.sh
+++ .platform/hooks/prebuild/00_install_dependencies.sh
@@ -21,7 +21,7 @@ source /var/app/venv/*/bin/activate
 # --------------------------------------------
 # You can install packages directly here via pip or install from requirements.txt.
 # Here we install Django and psycopg2-binary (which uses the libpq headers above).
-pip install Django psycopg2-binary
+pip install --upgrade pip setuptools wheel
 
 # If a requirements.txt file exists, install any additional packages.
 if [ -f /var/app/current/requirements.txt ]; then
